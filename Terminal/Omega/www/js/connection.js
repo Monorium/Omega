75795b20d00946859dc6bacfc244e05057c98d0a
monorium <monorium.master@gmail.com>
2018-06-08 16:32:31 +0900

    Wi-Fiで6サーボ制御を実装

--- Terminal/Omega/www/js/connection.js
+++ Terminal/Omega/www/js/connection.js
@@ -1,12 +1,12 @@
 class WebSocketConnection {
+
   constructor() {
-    this._timer = null;
-    this._connected = false;
-    this._sock = new WebSocket("ws://192.168.3.201/omega");
-    this._sock.onopen = this.onOpen;
-    this._sock.onclose = this.onClose;
-    this._sock.onmessage = this.onMessage;
-    this._sock.onerror = this.onError;
+    this.connected = false;
+    this.sock = new WebSocket("ws://192.168.3.201/omega");
+    this.sock.addEventListener("open", this.onOpen.bind(this));
+    this.sock.addEventListener("close", this.onClose.bind(this));
+    this.sock.addEventListener("error", this.onError.bind(this));
+    this.sock.addEventListener("message", this.onMessage.bind(this));
   }
 
   // { "id": X, "angle": XX }
@@ -20,8 +20,20 @@ class WebSocketConnection {
   // 接続イベント
   onOpen() {
     console.log("Socket open");
-    this._connected = true;
-    this._timer = setInterval(this.healthCheckTimer, 3000);
+    this.connected = true;
+    console.log("connected:" + this.connected);
+  }
+
+  // 切断イベント
+  onClose() {
+    console.log("Socket close");
+    this.connected = false;
+  }
+
+  // エラーイベント
+  onError(event) {
+    console.log("エラーが発生しました。");
+    this.sock.close();
   }
 
   // メッセージ受信イベント
@@ -33,32 +45,24 @@ class WebSocketConnection {
     }
   }
 
-  // 切断イベント
-  onClose() {
-    console.log("Socket close");
-  }
-
-  // エラーイベント
-  onError(event) {
-    console.log("エラーが発生しました。");
-  }
-
   // ヘルスチェック送信タイマー処理
-  healthCheckTimer() {
-    console.log("Do health check");
-    if (this._connected) this._sock.send("ping");
+  sendPing() {
+    console.log("Send ping");
+    console.log("connected:" + this.connected);
+    if (this.connected) this.sock.send("ping");
   }
 
   // 個別制御電文送信
   sendManualControl(speed, servoList) {
-    console.log("Send manual control:" + this._connected);
-    // if (this._connected) {
+    console.log("Send manual control");
+    console.log("connected:" + this.connected);
+    // if (this.connected) {
       let data = new Object();
       data.action = "manual";
       data.speed = speed;
       data.servo = servoList;
       let jsonText = JSON.stringify(data);
-      this._sock.send(jsonText);
+      this.sock.send(jsonText);
       console.log("Sent !");
     // }
   }
